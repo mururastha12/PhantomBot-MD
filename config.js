0cf516abb4d0cc2ac919968f23d4524d87e95fe4
Phantom0 <94726697+opphantom0@users.noreply.github.com>
2022-04-10 07:57:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['916909137213','6283839980451'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['916909137213'] //another owner number
-global.premium = ['916909137213'] //premium number
-global.pengguna = 'Xeon' //username
-global.botnma = 'Cheems Bot-MD' //bot name
-global.ownernma = '🦄Dream Guy Xeon' //owner name
-global.packname = 'Cheems Bot' //sticker package name
-global.author = 'By Dream Guy Xeon' //sticker author name
+global.owner = ['919744037374','99999999'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['919744037374'] //another owner number
+global.premium = ['919744037374'] //premium number
+global.pengguna = 'Razi' //username
+global.botnma = 'Phantom Bot-MD' //bot name
+global.ownernma = '🦄Dream Guy Razi' //owner name
+global.packname = 'Phantom Bot' //sticker package name
+global.author = 'By Razi' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '⭔' //design
